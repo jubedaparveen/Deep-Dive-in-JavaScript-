6a2c4cd7782e1457aaa71c8e4c3d8d6f49fad464
Jubedaparveenh <jubedaparveenh@gmail.com>
2026-03-05 00:05:58 +0530

    1- To Do  project

---
+++ javascripts-learning/domm/dompr/myjsfile.js
@@ -0,0 +1,11 @@
+
+
+
+
+// console.log(window)
+// console.log(window.alert("Hello World!" + " Welcome to JavaScript!"))
+
+// console.log(window.document)
+
+// console.log(window.document.getElementById('changeColorBtn'))
+
